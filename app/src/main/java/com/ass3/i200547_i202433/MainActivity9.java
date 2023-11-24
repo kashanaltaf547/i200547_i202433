74a9aeb6374613e4c5d21715ef48c7446efb2490
KashanAltaf <kashanaltaf137@gmail.com>
2023-11-24 16:38:15 +0500

    Assignment3 Commit

--- app/src/main/java/com/ass3/i200547_i202433/MainActivity9.java
+++ app/src/main/java/com/ass3/i200547_i202433/MainActivity9.java
@@ -1,23 +1,24 @@
 package com.ass3.i200547_i202433;
 
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
 
 public class MainActivity9 extends AppCompatActivity {
 
@@ -34,83 +35,88 @@ public class MainActivity9 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main9);
 
-        l1 = (ImageButton) findViewById(R.id.image);
-        l2 = (ImageButton) findViewById(R.id.call);
-        l3 = (ImageButton) findViewById(R.id.video);
-        l4 = (ImageButton) findViewById(R.id.cam);
-        l5 = (ImageButton) findViewById(R.id.phot);
-        l6 = (ImageButton) findViewById(R.id.file);
-        r1 = (EditText) findViewById(R.id.reply);
-
+        l1 = findViewById(R.id.image);
+        l2 = findViewById(R.id.call);
+        l3 = findViewById(R.id.video);
+        l4 = findViewById(R.id.cam);
+        l5 = findViewById(R.id.phot);
+        l6 = findViewById(R.id.file);
+        r1 = findViewById(R.id.reply);
 
         l1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Activity1();
             }
         });
         l2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Activity2();
             }
         });
         l3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Activity3();
             }
         });
         l4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Activity4();
             }
         });
         l5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Activity5();
+                sendMultimediaMessage("senderId", "receiverId", "This is a photo message", new File("path_to_photo"));
             }
         });
         l6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Activity6();
+                // Handle file button click
+                // You may want to add code to select and send a file
+                sendMultimediaMessage("senderId", "receiverId", "This is a file message", new File("path_to_file"));
             }
         });
-
-
     }
 
-    private void Activity6() {
-        Intent intent = new Intent(this, MainActivity17.class);
-        startActivity(intent);
-    }
+    private void sendMultimediaMessage(final String senderId, final String receiverId, final String message, final File file) {
+        // AsyncTask to send multimedia message in the background
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                String apiUrl = "http://localhost/sendMessage.php";
+                try {
 
-    private void Activity5() {
-        Intent intent = new Intent(this, MainActivity16.class);
-        startActivity(intent);
-    }
+                    URL url = new URL(apiUrl);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-    private void Activity4() {
-        Intent intent = new Intent(this, MainActivity17.class);
-        startActivity(intent);
-    }
 
-    private void Activity3() {
-        Intent intent = new Intent(this, MainActivity15.class);
-        startActivity(intent);
-    }
+                    // Read the response from the server
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
 
-    private void Activity2() {
-        Intent intent = new Intent(this, MainActivity18.class);
-        startActivity(intent);
-    }
+                    JSONObject jsonResponse = new JSONObject(response.toString());
 
-    private void Activity1() {
-        Intent intent = new Intent(this, MainActivity8.class);
-        startActivity(intent);
-    }
+                    reader.close();
+                    connection.disconnect();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
 
+            @Override
+            protected void onPostExecute(Void result) {
+                Toast.makeText(MainActivity9.this, "Multimedia message sent", Toast.LENGTH_SHORT).show();
+            }
+        }.execute();
+    }
 }
